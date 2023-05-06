4ab8368bd9d3ff647411af44ef6ddea2ff4c81ce
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-06 08:39:54 +0100

    commit

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,8 @@
 -- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
 
 -- Quantity in the table items can be negative.
-CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
+CREATE TRIGGER decrease_item_quantity AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE id = NEW.item_id;
+  UPDATE items SET quantity = quantity - NEW.quantity WHERE id = NEW.item_id;
 END;
