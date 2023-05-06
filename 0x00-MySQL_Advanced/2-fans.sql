7196b5650b567761bd1fea40911302690ada1094
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-06 09:18:19 +0100

    commit

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -6,8 +6,7 @@
 -- Column names must be: origin and nb_fans
 -- SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
 
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
-WHERE nb_fans >= 0
 GROUP BY origin
 ORDER BY nb_fans DESC;
