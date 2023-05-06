aa5ba48039f52519564ca9e35a86340c4f42a801
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-06 08:35:15 +0100

    fix #2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -5,8 +5,10 @@
 -- Import this table dump: metal_bands.sql.zip
 -- Column names must be: origin and nb_fans
 SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
 
 SELECT origin, SUM(nb_fans) AS nb_fans
 FROM metal_bands
+WHERE nb_fans >= 0
 GROUP BY origin
 ORDER BY nb_fans DESC;
