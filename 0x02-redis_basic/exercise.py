773c31b6ea7c4473f96b4931b571c0b3ff282e16
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-07-11 14:18:59 +0100

    Fix Cache.store

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -43,7 +43,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """_summary_
 
